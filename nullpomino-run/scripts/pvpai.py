912a55d07e564e5f2a3601b81c9b4baf61910266
Fatfat <richardfat7@gmail.com>
2018-10-04 16:48:35 +0800

    fixfix

--- nullpomino-run/scripts/pvpai.py
+++ nullpomino-run/scripts/pvpai.py
@@ -1,13 +1,55 @@
 import math
 
-class AIPlayer():
-    def __init__(self, sth):
+class PyAIPlayer():
+    def __init__(self, name):
+        self.name = name
         pass
-
-class PvPAI(AIPlayer):
-    def __init__(self):
+    
+    def newPiece(self, engine, playerID):
+        pass
+    
+    def onFirst(self, engine, playerID):
+        pass
+    
+    def onLast(self, engine, playerID):
+        pass
+    
+    def renderState(self, engine, playerID):
+        pass
+    
+    def setControl(self, engine, playerID, ctrl):
+        pass
+    
+    def shutdown(self, engine, playerID):
         pass
         
+    def renderHint(self, engine, playerID):
+        pass
+
+
+class PvPAI(PyAIPlayer):
+    def __init__(self, name):
+        self.name = name
+        pass
+    
     def newPiece(self, engine, playerID):
-        return 1
+        pass
+    
+    def onFirst(self, engine, playerID):
+        pass
+    
+    def onLast(self, engine, playerID):
+        pass
+    
+    def renderState(self, engine, playerID):
+        pass
+    
+    def setControl(self, engine, playerID, ctrl):
+        pass
+    
+    def shutdown(self, engine, playerID):
+        pass
+        
+    def renderHint(self, engine, playerID):
+        pass
 
